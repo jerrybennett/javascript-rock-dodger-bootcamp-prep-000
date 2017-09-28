a0b19f8e2e495c9c29049dd694e4d4a86ae3af48
Jerry Bennett <jerrysbusiness@gmail.com>
2017-09-28 18:09:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ function moveDodgerLeft() {
    window.requestAnimationFrame(function() {
      var left = dodgerLeftEdge;
      if(left > 0) {
-       left = `${left - 4}px`;
+       dodgerLeftEdge = `${left - 4}px`;
      }
    });
 }
@@ -165,7 +165,7 @@ function moveDodgerRight() {
    window.requestAnimationFrame(function() {
      var right = dodgerRightEdge;
      if(right < 360) {
-       right = `${right + 4}px`;
+       dodgerRightEdge = `${right + 4}px`;
      }
    });
 }
