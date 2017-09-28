e890c3097a3b72f90faea633a0f3ffd5dfba75bc
Jerry Bennett <jerrysbusiness@gmail.com>
2017-09-28 19:31:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function createRock(x) {
   function moveRock() {
     // implement me!
     // (use the comments below to guide you!)
-    rock.style.top = parseInt(top) + 2;
+    rock.style.top = `${top + 2}px`;
 
     /**
      * If a rock collides with the DODGER,
